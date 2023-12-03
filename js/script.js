8fc501380da4cc99695e08f2010e102c7eaccd91
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2023-12-03 21:41:34 +0700

    rapihin js lekk

--- js/script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('proposal').addEventListener('click', function() {
-    window.location.href = '../proposal.html';
-});
--- js/template/template.js
+++
@@ -1,2 +0,0 @@
-export let URLRegister = `https://asia-southeast2-wegotour-403712.cloudfunctions.net/wegotour-registeruser`
-export let token = 'token';
